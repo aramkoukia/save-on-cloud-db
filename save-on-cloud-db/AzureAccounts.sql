1da31c5fa0dba841a0f8ec22328f2f8f087295da
Aram Koukia <aramkoukia@gmail.com>
2020-04-03 15:54:53 -0700

    adding azure tables

---
+++ save-on-cloud-db/AzureAccounts.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[AzureAccounts](
+	[Id] INT NOT NULL IDENTITY,
+	[TenantId] [nvarchar](500) NOT NULL,
+	[SubscriptionId] [nvarchar](500) NOT NULL,
+	[ClientId] [nvarchar](500) NOT NULL,
+	[ClientSecret] [nvarchar](500) NOT NULL
+    PRIMARY KEY ([Id])
+)
---
+++ save-on-cloud-db/UserAzureAccounts.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE [dbo].[UserAzureAccounts](
+	[UserId] [nvarchar](450) NOT NULL,
+	[AzureAccountId] INT NOT NULL
+	PRIMARY KEY ([UserId], [AzureAccountId])
+)
