039ff77f66f736c3e7eca81444d4661a62e68999
Aram Koukia <aramkoukia@gmail.com>
2020-04-16 17:56:27 -0700

    added org table

---
+++ save-on-cloud-db/Organizations.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Organizations]
+(
+	[Id] INT NOT NULL IDENTITY,
+	CompanyName [nvarchar](500) NOT NULL,
+	Country [nvarchar](500) NOT NULL,
+	Privince [nvarchar](500) NOT NULL,
+	City [nvarchar](500) NOT NULL,
+	[Address] [nvarchar](500) NOT NULL,
+	PostalCode [nvarchar](500) NOT NULL,
+	Email [nvarchar](500) NOT NULL
+	PRIMARY KEY ([Id])
+)
